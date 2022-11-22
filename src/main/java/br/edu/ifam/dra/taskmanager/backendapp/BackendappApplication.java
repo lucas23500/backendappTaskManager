0f1f31af7dff53b6d12c4d3497e9840f9077856b
Lucas <lucas23500@gmail.com>
2022-11-21 20:56:02 -0400

    Commitando Updates Basicas

---
+++ src/main/java/br/edu/ifam/dra/taskmanager/backendapp/BackendappApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifam.dra.taskmanager.backendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifam/dra/taskmanager/backendapp/BackendappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifam.dra.taskmanager.backendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
